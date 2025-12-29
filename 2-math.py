51fc3fec17a0abf0b1f95fd06ef5fffa1cc08e4a
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-29 19:49:54 -0300

    Usando o math no python

--- 2-math.py
+++ 2-math.py
@@ -2,8 +2,16 @@ import math
 # Acessar o número PI
 print(math.pi)
 print(f"{math.pi:.2f}")
-
+print("---------------------------------------")
 # Acessar o número de Euler
 print(math.e)
 print(f"{math.e:.2f}")
-
+print("---------------------------------------")
+# Arredondando o número para cima e para baixo.
+num = 10.4
+print(math.ceil(num))
+print(math.floor(num))
+print("---------------------------------------")
+# Calcular um fatorial de um número
+num1 = int(input("Digite um número: \n"))
+print(math.factorial(num1))
