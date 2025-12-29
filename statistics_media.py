00166de73f69b61bde453c1195d0e8f44856e159
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-29 20:22:31 -0300

    A mediana dos números

---
+++ statistics_media.py
@@ -0,0 +1,7 @@
+import statistics
+# Aplicando a média 
+print(f"A média é: {statistics.mean([3,2,3,8,9])}")
+print('-------------------------------------------------')
+# Aplicando a médiana
+print(f"A mediana é: {statistics.median([1, 2, 4, 8, 9])}")
+print(f"A mediana é: {statistics.median([1,2,3,7,8,9])}")
