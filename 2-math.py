97deb534bb33740bcc651d42d31356be301cc6f6
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-29 20:01:26 -0300

    Usando o math e suas operações

--- 2-math.py
+++ 2-math.py
@@ -15,3 +15,16 @@ print("---------------------------------------")
 # Calcular um fatorial de um número
 num1 = int(input("Digite um número: \n"))
 print(math.factorial(num1))
+print("---------------------------------------")
+# Calcular um fatorial de um números
+print(math.pow(5,5))
+print("---------------------------------------")
+# Raiz quadrada de um número
+print(math.sqrt(169))
+print("---------------------------------------")
+# MDC
+mdc = math.gcd(20,100)
+print(mdc)
+print("---------------------------------------")
+# Logaritimo
+print(math.log(10))
