a63953dc87b86661f8107d5b2a1d13904f2e9bcb
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-29 09:26:33 -0300

    Usando o Módulo - OS com liagameto e desligamento do Notebool

--- 1-Os.py
+++ 1-Os.py
@@ -9,7 +9,22 @@ os.system('ver')
 os.system('systeminfo')
 # Limpar a tela do terminal
 os.system('cls')
+#1- Desligar o imediatamente
+# os.system('shutdown/s/t 0')
+#2- Desligar o computador
+# os.system('shutdown/s')
+#3- Cancela o desligamento
+# os.system('shutdown/a')
 
+def DesligaremUmaHora():
+    os.system('shutdown /s /t 3600')    
+
+def DesligarEmMeiaHora():
+    os.system(('shutdown /s /t 1800'))
+
+def CancelaShutDown():
+    os.system(('shutdown /a'))
+CancelaShutDown()
 
 
 
