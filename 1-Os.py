1826ac3d1f80d35f54bfa53f91b7e1fbe0b4608b
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-29 09:11:19 -0300

    Usando o Módulo - OS

---
+++ 1-Os.py
@@ -0,0 +1,15 @@
+import os
+# Consultar as funcionalidades do Módulo OS- (Retornar a pasta atual):
+print(os.getcwd)
+# Listar arquivos e pastas
+print(os.listdir)
+# Verificar a versão do sistema Operacinal
+os.system('ver')
+# Configurações da máquina
+os.system('systeminfo')
+# Limpar a tela do terminal
+os.system('cls')
+
+
+
+
--- Primeiro Módulo/main.py
+++ Primeiro Módulo/main.py
@@ -12,7 +12,7 @@ print(math_operation.divide(5,3))
 print(multiply(6,5))
 print(divide(6,6))
 
-#Coloca em maiúculo.
+#Coloca em maiúculo
 print(strings_stils.capitalize("hello"))
 #Inverte as letras.
 print(strings_stils.reverse_strings("Python"))
