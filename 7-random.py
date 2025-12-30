21c49a1f99776727adfd320da4408dce6960f576
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-29 23:31:04 -0300

    Módulos - Random - Letras  aleatórios

--- 7-random.py
+++ 7-random.py
@@ -1,4 +1,15 @@
 import random
-#Selecione um valor aleatório de uma lista
+# 1-Selecione um valor aleatório de uma lista
 lista1 = [7,6,4,3,2,1]
 print(random.choice(lista1))
+print("----------------------------")
+# 2-Gerar um número aleatório em um intervalo de valores
+r1 = random.randint(5,15)
+print(r1)
+print("----------------------------")
+# 4-Selecionar um caractere aleatório de uma string
+name = "Curso python"
+r2 = random.choice(name)
+print(r2)
+
+
